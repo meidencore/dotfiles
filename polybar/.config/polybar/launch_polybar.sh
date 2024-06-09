fc0c5c0ae0ce8ec630a633ad32b7e7dcc245952c
meidencore <danieleverdale@gmail.com>
2024-06-09 17:48:22 -0400

    initial dotfiles

---
+++ polybar/.config/polybar/launch_polybar.sh
@@ -0,0 +1,10 @@
+killall -q polybar
+#polybar toph 2>&1 | tee -a /tmp/polybar.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload toph &
+  done
+else
+  polybar --reload toph &
+fi
