cd359add66464f99e6d95851e97fc3a4c646320b
meidencore <danieleverdale@gmail.com>
2024-10-12 14:07:50 -0400

    added telescope config to ignore node-modules && ignore packer_compiled

--- nvim/.config/nvim/lua/meidencore/packer.lua
+++ nvim/.config/nvim/lua/meidencore/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
 	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'} }
   }
 
 --  use ({ 'rose-pine/neovim', as = 'rose-pine' })
